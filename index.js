df6e4b1b97ad8bf02d81312c4b2281e097ae6a51
Le H Ngo <seiji@o2.pl>
2019-05-08 14:38:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, {[key]: value})
 }
-
-test = updateObjectWithKeyAndValue(recipes, "Money", 1)
-
-console.log(test)
